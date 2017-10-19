c01c8cccf394d2394e12eae1b6365dc22cd19f46
westwebdev <anatolij.ihnato@gmail.com>
2017-10-19 15:29:49 +0300

    a

---
+++ autokrovatki/send.php
@@ -0,0 +1,88 @@
+<?php
+/*
+If you see this text in your browser, PHP is not configured correctly on this webhost.
+Contact your hosting provider regarding PHP configuration for your site.
+*/
+
+require_once('form_throttle.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	if (formthrottle_too_many_submissions($_SERVER["REMOTE_ADDR"]))
+	{
+		echo '{"MusePHPFormResponse": { "success": false,"error": "Too many recent submissions from this IP"}}';
+	}
+	else
+	{
+		emailFormSubmission();
+	}
+}
+
+
+
+function emailFormSubmission()
+{
+	$to = '0632066116@ukr.net';
+	$headers = 'From: admin@avtokrovatki.zzz.com.ua' . "\r\n";
+	$headers.= 'MIME-Version: 1.0' . "\r\n";
+	$headers.= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$subject = 'Заявка с сайта ' . htmlentities($_SERVER["SERVER_NAME"],ENT_COMPAT,'UTF-8');
+
+	$message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/><title>' . htmlentities($subject,ENT_COMPAT,'UTF-8') . '</title></head>';
+	$message .= '<body style="background-color: #ffffff; color: #000000; font-style: normal; font-variant: normal; font-weight: normal; font-size: 12px; line-height: 18px; font-family: helvetica, arial, verdana, sans-serif;">';
+	$message .= '<h2 style="background-color: #eeeeee;">Посланы новые данные</h2><table cellspacing="0" cellpadding="0" width="100%" style="background-color: #ffffff;">';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Форма:</b></td><td>' . htmlentities($_REQUEST["ordername"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Имя:</b></td><td>' . htmlentities($_REQUEST["name"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Телефон:</b></td><td>' . htmlentities($_REQUEST["phone"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Серия:</b></td><td>' . htmlentities($_REQUEST["series"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Модель:</b></td><td>' . htmlentities($_REQUEST["model"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Цена:</b></td><td>' . htmlentities($_REQUEST["price"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Цвет:</b></td><td>' . htmlentities($_REQUEST["color"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Доп. опции:</b></td><td>' . htmlentities($_REQUEST["add"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+
+
+
+
+
+
+	$message .= '</table><br/><br/>';
+	$message .= '<div style="background-color: #eeeeee; font-size: 10px; line-height: 11px;">Форма прислана с сайта: ' . htmlentities($_SERVER["SERVER_NAME"],ENT_COMPAT,'UTF-8') . '</div>';
+	$message .= '<div style="background-color: #eeeeee; font-size: 10px; line-height: 11px;">Visitor IP address: ' . htmlentities($_SERVER["REMOTE_ADDR"],ENT_COMPAT,'UTF-8') . '</div>';
+	$message .= '</body></html>';
+	$message = cleanupMessage($message);
+
+	$formEmail = cleanupEmail($_REQUEST['Email']);
+
+	$sent = @mail($to, $subject, $message, $headers);
+
+	if($sent)
+	{
+
+	    //header('Refresh: 0; URL=thanks.html');
+		//echo '{"FormResponse": {"success": true, "redirect": "thanks.html"}}';
+
+
+		header('Location: /');
+	   exit;
+
+
+	}
+	else
+	{
+		echo '{"MusePHPFormResponse": { "success": false,"error": "Failed to send email"}}';
+	}
+}
+
+function cleanupEmail($email)
+{
+	$email = htmlentities($email,ENT_COMPAT,'UTF-8');
+	$email = preg_replace('=((<CR>|<LF>|0x0A/%0A|0x0D/%0D|\\n|\\r)\S).*=i', null, $email);
+	return $email;
+}
+
+function cleanupMessage($message)
+{
+	$message = wordwrap($message, 70, "\r\n");
+	return $message;
+}
+?>
