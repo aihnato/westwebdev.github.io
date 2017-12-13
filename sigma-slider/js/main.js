39f13175b1fa67bee49bbab154cb79583e454342
westwebdev <anatolij.ihnato@gmail.com>
2017-12-13 14:43:23 +0200

    !

--- sigma-slider/js/main.js
+++ sigma-slider/js/main.js
@@ -1,16 +1,15 @@
-$(function() {
-	var koefHeig = $('.carousel-cell').children().height() * 25 / 100;
-	$('.carousel').height($('.carousel-cell').children().height() + koefHeig);
-	$(window).resize(function(){
-		$('.carousel').height($('.carousel-cell').children().height() + koefHeig);
-	});
+// $(function() {
+// 	var koefHeig = $('.carousel-cell').children().height() * 30 / 100;
+// 	$('.carousel').height($('.carousel-cell').children().height() + koefHeig);
+// 	$(window).resize(function(){
+// 		$('.carousel').height($('.carousel-cell').children().height() + koefHeig);
+// 	});
+// });
 
-
-});
 $(function() {
-	$('.device-block').height($('.device-block').children().height());
+	$('.device-block').height($('.device-block').children().height() + 5);
 	$(window).resize(function(){
-		$('.device-block').height($('.device-block').children().height());
+		$('.device-block').height($('.device-block').children().height() + 5);
 	});
 });
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZnVuY3Rpb24oKSB7XHJcblx0dmFyIGtvZWZIZWlnID0gJCgnLmNhcm91c2VsLWNlbGwnKS5jaGlsZHJlbigpLmhlaWdodCgpICogMjUgLyAxMDA7XHJcblx0JCgnLmNhcm91c2VsJykuaGVpZ2h0KCQoJy5jYXJvdXNlbC1jZWxsJykuY2hpbGRyZW4oKS5oZWlnaHQoKSArIGtvZWZIZWlnKTtcclxuXHQkKHdpbmRvdykucmVzaXplKGZ1bmN0aW9uKCl7XHJcblx0XHQkKCcuY2Fyb3VzZWwnKS5oZWlnaHQoJCgnLmNhcm91c2VsLWNlbGwnKS5jaGlsZHJlbigpLmhlaWdodCgpICsga29lZkhlaWcpO1xyXG5cdH0pO1xyXG5cclxuXHJcbn0pO1xyXG4kKGZ1bmN0aW9uKCkge1xyXG5cdCQoJy5kZXZpY2UtYmxvY2snKS5oZWlnaHQoJCgnLmRldmljZS1ibG9jaycpLmNoaWxkcmVuKCkuaGVpZ2h0KCkpO1xyXG5cdCQod2luZG93KS5yZXNpemUoZnVuY3Rpb24oKXtcclxuXHRcdCQoJy5kZXZpY2UtYmxvY2snKS5oZWlnaHQoJCgnLmRldmljZS1ibG9jaycpLmNoaWxkcmVuKCkuaGVpZ2h0KCkpO1xyXG5cdH0pO1xyXG59KTsiXSwiZmlsZSI6Im1haW4uanMifQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vICQoZnVuY3Rpb24oKSB7XHJcbi8vIFx0dmFyIGtvZWZIZWlnID0gJCgnLmNhcm91c2VsLWNlbGwnKS5jaGlsZHJlbigpLmhlaWdodCgpICogMzAgLyAxMDA7XHJcbi8vIFx0JCgnLmNhcm91c2VsJykuaGVpZ2h0KCQoJy5jYXJvdXNlbC1jZWxsJykuY2hpbGRyZW4oKS5oZWlnaHQoKSArIGtvZWZIZWlnKTtcclxuLy8gXHQkKHdpbmRvdykucmVzaXplKGZ1bmN0aW9uKCl7XHJcbi8vIFx0XHQkKCcuY2Fyb3VzZWwnKS5oZWlnaHQoJCgnLmNhcm91c2VsLWNlbGwnKS5jaGlsZHJlbigpLmhlaWdodCgpICsga29lZkhlaWcpO1xyXG4vLyBcdH0pO1xyXG4vLyB9KTtcclxuXHJcbiQoZnVuY3Rpb24oKSB7XHJcblx0JCgnLmRldmljZS1ibG9jaycpLmhlaWdodCgkKCcuZGV2aWNlLWJsb2NrJykuY2hpbGRyZW4oKS5oZWlnaHQoKSArIDUpO1xyXG5cdCQod2luZG93KS5yZXNpemUoZnVuY3Rpb24oKXtcclxuXHRcdCQoJy5kZXZpY2UtYmxvY2snKS5oZWlnaHQoJCgnLmRldmljZS1ibG9jaycpLmNoaWxkcmVuKCkuaGVpZ2h0KCkgKyA1KTtcclxuXHR9KTtcclxufSk7Il0sImZpbGUiOiJtYWluLmpzIn0=
